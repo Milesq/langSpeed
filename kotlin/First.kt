3c04d50c5f10f11d07047613f08f286d406b9fa1
Milesq <wmilosz88@gmail.com>
2019-05-28 10:20:00 +0200

    Add kotlin

---
+++ kotlin/First.kt
@@ -0,0 +1,11 @@
+package First
+
+fun isFirst(x: Int): Boolean {
+    for (i in x / 2 + 1 downTo 2) {
+        if (x % i == 0) {
+            return false
+        }
+    }
+
+    return true
+}
---
+++ kotlin/Main.kt
@@ -0,0 +1,9 @@
+import First.isFirst
+
+fun main() {
+    for (i in 1..__ITERS__) {
+        isFirst(i)
+    }
+
+    println("end")
+}
