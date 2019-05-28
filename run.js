a816003048d1da0c7de17c212dc53a42be9f3548
Milesq <wmilosz88@gmail.com>
2019-05-28 08:34:05 +0200

    Add run programs

--- run.js
+++ run.js
@@ -3,11 +3,23 @@ let { exec } = require('child_process');
 exec = promisify(exec);
 const fs = require('fs');
 
-module.exports = config => {
-    config.forEach(async ({ name, run, dir }) => {
-        // const result = await exec(run, {
-        //     cwd: `.tmp/${dir}`
-        // });
-        console.log(fs.existsSync('.tmp/c/a.exe'));
+module.exports = async config => {
+    const ret = {};
+
+    const result = config.map(async ({ name, run, dir }) => {
+        let time = new Date().getTime();
+        const { stderr } = await exec(run, {
+            cwd: `.tmp/${dir}`
+        });
+        time = new Date().getTime() - time;
+
+        if(stderr !== '') {
+            throw 'Unexcepted stderr from program! ' + stderr;
+        }
+
+        ret[name] = time;
     });
+
+    await Promise.all(result);
+    return ret;
 };
