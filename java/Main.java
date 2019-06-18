1114e2d7f1a8c2e58b4fb29955ba1e19000e5d48
Milesq <wmilosz88@gmail.com>
2019-06-18 14:52:35 +0200

    Add java factorial

--- java/Main.java
+++ java/Main.java
@@ -1,18 +1,17 @@
 public class Main {
     public static void main(String args[]) {
         for (int i=__ITERS__;i>1;--i) {
-            isFirst(i);
+            factorial(i);
         }
 
         System.out.println("end");
     }
 
-    public static boolean isFirst(int number) {
-        for(int i=2;i <= number / 2;++i) {
-            if (number % i == 0)
-                return false;
+    public static int factorial(int number) {
+        if (number == 2) {
+            return 2;
+        } else {
+            return number * factorial(number - 1);
         }
-
-        return true;
     }
 }
