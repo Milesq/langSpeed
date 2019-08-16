53268af20a30dca1a35b886f49715d3eb289082e
Milesq <wmilosz88@gmail.com>
2019-08-16 22:56:19 +0200

    Sort results

--- timer.js
+++ timer.js
@@ -1,34 +1,37 @@
-const fs = require('fs');
-const languages = require('./config.json');
-const transpile = require('./transpile');
-const compile = require('./compile');
-const run = require('./run');
+const fs = require("fs");
+const languages = require("./config.json");
+const transpile = require("./transpile");
+const compile = require("./compile");
+const run = require("./run");
 
 languages.forEach(({ run, dir, main }) => {
-  if(run === undefined || dir === undefined || main === undefined) {
-    throw new Error;
-  }
+    if (run === undefined || dir === undefined || main === undefined) {
+        throw new Error();
+    }
 });
 
 const iters = process.argv[2] || 50000;
 
 const config = languages.map(userConfig => {
-  return {
-    iters,
-    name: userConfig.dir + '-time',
-    ...userConfig
-  }
+    return {
+        iters,
+        name: userConfig.dir + "-time",
+        ...userConfig
+    };
 });
 
 (async () => {
-  console.log('Generating files...');
-  await transpile(config);
+    console.log("Generating files...");
+    await transpile(config);
 
-  console.log('Compiling...');
-  await compile(config);
+    console.log("Compiling...");
+    await compile(config);
 
-  console.log('Run');
-  const times = await run(config);
+    console.log("Run");
+    let times = await run(config);
+    times = Object.entries(times);
+    times.sort((x, y) => (x[1] == y[1] ? 0 : x[1] > y[1] ? 1 : -1));
+    times = Object.fromEntries(times);
 
-  fs.writeFileSync('results.json', JSON.stringify(times, null, 2));
+    fs.writeFileSync("results.json", JSON.stringify(times, null, 2));
 })();
