826d2dd2bf8507840b28ae59f7341bf0a793e59c
Milesq <wmilosz88@gmail.com>
2020-08-30 10:26:43 +0200

    add denojs

---
+++ denojs/isPrime.js
@@ -0,0 +1,8 @@
+export default number => {
+    for (let i = 2; i <= number / 2; ++i) {
+        if (number % i == 0)
+            return false;
+    }
+
+    return true;
+};
---
+++ denojs/main.js
@@ -0,0 +1,7 @@
+import isPrime from './isPrime.js';
+
+for (let i = __ITERS__; i > 1; --i) {
+    isPrime(i);
+}
+
+console.log("end");
